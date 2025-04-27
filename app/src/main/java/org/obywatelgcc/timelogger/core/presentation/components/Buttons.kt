750997ad5f07259d437ab7d332117454cf66d997
Krystian Kałużny <k.kaluzny141@gmail.com>
2025-04-27 16:04:00 +0200

    Add custom buttons

---
+++ app/src/main/java/org/obywatelgcc/timelogger/core/presentation/components/Buttons.kt
@@ -0,0 +1,151 @@
+package org.obywatelgcc.timelogger.core.presentation.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object CustomButtonDefaults {
+
+    val contentModifier = Modifier
+        .defaultMinSize(minWidth = ButtonDefaults.MinWidth, minHeight = ButtonDefaults.MinHeight)
+        .padding(ButtonDefaults.ContentPadding)
+
+    val textButtonContentModifier = Modifier
+        .defaultMinSize(minWidth = ButtonDefaults.MinWidth / 2, minHeight = ButtonDefaults.MinHeight / 2)
+        .padding(ButtonDefaults.TextButtonContentPadding)
+
+    val minimumInteractiveComponentSize = 25.dp
+
+    fun Modifier.textButtonContentModifier(paddingValues: PaddingValues) = this
+        .defaultMinSize(minWidth = ButtonDefaults.MinWidth / 2, minHeight = ButtonDefaults.MinHeight / 2)
+        .padding(paddingValues)
+
+    fun Modifier.textButtonContentModifier(all: Dp) = this
+        .defaultMinSize(minWidth = ButtonDefaults.MinWidth / 2, minHeight = ButtonDefaults.MinHeight / 2)
+        .padding(all)
+
+    fun Modifier.textButtonContentModifier(horizontal: Dp = 0.dp, vertical: Dp = 0.dp) = this
+        .defaultMinSize(minWidth = ButtonDefaults.MinWidth / 2, minHeight = ButtonDefaults.MinHeight / 2)
+        .padding(horizontal, vertical)
+}
+
+@Composable
+fun Button(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = ButtonDefaults.shape,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+//    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+    border: BorderStroke? = null,
+    contentModifier: Modifier = CustomButtonDefaults.contentModifier,
+    interactionSource: MutableInteractionSource? = null,
+    content: @Composable RowScope.() -> Unit
+) {
+    @Suppress("NAME_SHADOWING")
+    val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
+    val containerColor = colors.containerColor(enabled)
+    val contentColor = colors.contentColor(enabled)
+    val shadowElevation = 0.dp
+
+
+
+    CompositionLocalProvider(
+        LocalMinimumInteractiveComponentSize provides CustomButtonDefaults.minimumInteractiveComponentSize,
+    ) {
+        Surface(
+            onClick = onClick,
+            modifier = modifier.semantics { role = Role.Button },
+            enabled = enabled,
+            shape = shape,
+            color = containerColor,
+            contentColor = contentColor,
+            shadowElevation = shadowElevation,
+            border = border,
+            interactionSource = interactionSource
+        ) {
+            ProvideContentColorTextStyle(
+                contentColor = contentColor,
+                textStyle = MaterialTheme.typography.labelLarge
+            ) {
+                Row(
+                    contentModifier,
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = content
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun TextButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = ButtonDefaults.textShape,
+    colors: ButtonColors = ButtonDefaults.textButtonColors(),
+//    elevation: ButtonElevation? = null,
+    border: BorderStroke? = null,
+    contentModifier: Modifier = CustomButtonDefaults.textButtonContentModifier,
+    interactionSource: MutableInteractionSource? = null,
+    content: @Composable RowScope.() -> Unit
+) =
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+//        elevation = elevation,
+        border = border,
+        contentModifier = contentModifier,
+        interactionSource = interactionSource,
+        content = content
+    )
+
+fun ButtonColors.containerColor(enabled: Boolean): Color =
+    if (enabled) containerColor else disabledContainerColor
+
+fun ButtonColors.contentColor(enabled: Boolean): Color =
+    if (enabled) contentColor else disabledContentColor
+
+@Composable
+internal fun ProvideContentColorTextStyle(
+    contentColor: Color,
+    textStyle: TextStyle,
+    content: @Composable () -> Unit
+) {
+    val mergedStyle = LocalTextStyle.current.merge(textStyle)
+    CompositionLocalProvider(
+        LocalContentColor provides contentColor,
+        LocalTextStyle provides mergedStyle,
+        content = content
+    )
+}
--- app/src/main/java/org/obywatelgcc/timelogger/timer/presentation/components/DateTimePickerCompose.kt
+++ app/src/main/java/org/obywatelgcc/timelogger/timer/presentation/components/DateTimePickerCompose.kt
@@ -14,17 +14,20 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.material3.rememberTimePickerState
@@ -35,17 +38,155 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
+import org.obywatelgcc.timelogger.core.presentation.components.CustomButtonDefaults.textButtonContentModifier
+import org.obywatelgcc.timelogger.core.presentation.components.TextButton
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+
+@Preview
+@Composable
+fun DateTimePickerRowPreview() {
+
+    DateTimePickerRow2("test", LocalDateTime.now())
+}
+
+@Composable
+fun DateTimePickerRow2(
+    title: String,
+    initTimeDate: LocalDateTime,
+    onDateSelected: (LocalDate) -> Unit = {},
+    onTimeSelected: (LocalTime) -> Unit = {}
+) {
+
+    val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+    val initDate = initTimeDate.toLocalDate()
+    val initTime = initTimeDate.toLocalTime()
+
+    var timerDataTimeCheckState by remember { mutableStateOf(TimerDateTimeCheckState()) }
+
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(start = 10.dp, bottom = 10.dp, end = 10.dp, top = 40.dp)
+            .fillMaxWidth()
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(1.0f)
+        ) {
+
+            CheckableTextButton(timerDataTimeCheckState.startDateChecked, initDate.format(dateFormatter)) {
+                timerDataTimeCheckState = timerDataTimeCheckState.toggleStartDate()
+            }
+
+            CheckableTextButton(timerDataTimeCheckState.startTimeChecked, initTime.format(timeFormatter)) {
+                timerDataTimeCheckState = timerDataTimeCheckState.toggleStartTime()
+            }
+        }
+
+        Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "Arrow")
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(1.0f)
+        ) {
+            CheckableTextButton(timerDataTimeCheckState.endDateChecked, initDate.format(dateFormatter)) {
+                timerDataTimeCheckState = timerDataTimeCheckState.toggleEndDate()
+            }
+
+            CheckableTextButton(timerDataTimeCheckState.endTimeChecked, initTime.format(timeFormatter)) {
+                timerDataTimeCheckState = timerDataTimeCheckState.toggleEndTime()
+            }
+        }
+    }
+
+
+//
+//    ElevatedCard(
+//        modifier = Modifier
+//            .padding(horizontal = 10.dp)
+//            .fillMaxWidth(1f),
+//        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+//    ) {
+//        Text(
+//            text = title,
+//            modifier = Modifier.padding(start = 10.dp, top = 5.dp),
+//            fontSize = LocalTextStyle.current.fontSize
+//        )
+//        Row(
+//            verticalAlignment = Alignment.CenterVertically,
+//            modifier = Modifier.padding(start = 10.dp, bottom = 10.dp, end = 10.dp)
+//        ) {
+//            TextFiledWithPicker(
+//                value = initDate.format(dateFormatter),
+//                modifier = Modifier.weight(4f),
+//                trailingIcon = {
+//                    Icon(Icons.Default.DateRange, contentDescription = "Select date")
+//                },
+//            ) { onDismiss ->
+//                DatePickerDialog(onDateSelected = onDateSelected, onDismiss = onDismiss)
+//            }
+//
+//            Spacer(Modifier.width(16.dp))
+//
+//            TextFiledWithPicker(
+//                value = initTime.format(timeFormatter),
+//                modifier = Modifier.weight(3f),
+//                trailingIcon = {
+//                    Icon(Icons.Default.Clock, contentDescription = "Select time")
+//                },
+//            ) { onDismiss ->
+//                TimePickerDialog(
+//                    initTime = initTime, onTimeSelected = onTimeSelected, onDismiss = onDismiss
+//                )
+//            }
+//        }
+//    }
+}
+
+private data class TimerDateTimeCheckState(
+    val startDateChecked: Boolean = false,
+    val startTimeChecked: Boolean = false,
+    val endDateChecked: Boolean = false,
+    val endTimeChecked: Boolean = false
+) {
+    fun toggleStartDate(): TimerDateTimeCheckState = TimerDateTimeCheckState(startDateChecked = !startDateChecked)
+    fun toggleStartTime(): TimerDateTimeCheckState = TimerDateTimeCheckState(startTimeChecked = !startTimeChecked)
+    fun toggleEndDate(): TimerDateTimeCheckState = TimerDateTimeCheckState(endDateChecked = !endDateChecked)
+    fun toggleEndTime(): TimerDateTimeCheckState = TimerDateTimeCheckState(endTimeChecked = !endTimeChecked)
+}
+
+@Composable
+fun CheckableTextButton(checked: Boolean, text: String, onClick: () -> Unit) {
+    val contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+    val containerColor = if (checked) MaterialTheme.colorScheme.primaryContainer else Color.Transparent
+
+    TextButton(
+        modifier = Modifier.padding(2.dp),
+        contentModifier = Modifier.textButtonContentModifier(horizontal = 10.dp, vertical = 4.dp),
+        onClick = onClick,
+        colors =
+            ButtonDefaults.textButtonColors(
+                contentColor = contentColor,
+                containerColor = containerColor
+            )
+    ) {
+        Text(text = text)
+    }
+}
 
 
 @Composable
