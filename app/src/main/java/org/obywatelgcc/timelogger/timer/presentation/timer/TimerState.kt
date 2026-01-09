4262d34d67680fe301207c7a29ab7cae24758f11
Krystian Kałużny <k.kaluzny141@gmail.com>
2026-01-09 20:51:15 +0100

    Fix: Show negative duration when end time is before start time

--- app/src/main/java/org/obywatelgcc/timelogger/timer/presentation/timer/TimerState.kt
+++ app/src/main/java/org/obywatelgcc/timelogger/timer/presentation/timer/TimerState.kt
@@ -3,6 +3,7 @@ package org.obywatelgcc.timelogger.timer.presentation.timer
 import java.time.Duration
 import java.time.LocalDateTime
 import java.util.Locale
+import kotlin.math.absoluteValue
 
 data class TimerState(
     val runningState: RunningState = RunningState.READY_TO_START,
@@ -11,14 +12,16 @@ data class TimerState(
 ) {
 
     val durationStr: String by lazy {
-        val durationSeconds = Duration.between(startDateTime, endDateTime).seconds
+        val duration = Duration.between(startDateTime, endDateTime)
+        val durationSeconds = duration.seconds.absoluteValue
         val hoursPart = durationSeconds / 3600
         val minutesPart = (durationSeconds / 60) % 60
         val secondsPart = durationSeconds % 60
 
         String.format(
             Locale.getDefault(Locale.Category.FORMAT),
-            "%d:%02d:%02d",
+            "%s%d:%02d:%02d",
+            if (duration.isNegative) "-" else "",
             hoursPart,
             minutesPart,
             secondsPart
